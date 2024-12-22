8d65acd33c84c01da1b3a73aebcf9cfeeb1f4119
RocketKevin <kevinchen91200@gmail.com>
2024-12-21 21:02:50 -0600

    Added Readme and env example files

--- Setting/botConfigs.js
+++ Setting/botConfigs.js
@@ -4,10 +4,12 @@ const TIMEOUT = 120000;
 const RETRY_INTERVAL = 10000;
 const MAX_RETRIES = Math.floor(TIMEOUT / RETRY_INTERVAL);
 const TMUX_SESSION = 'GameServer';
+// The word "Minecraft" is used as a key when a user types "/startserver Minecraft".
+// Add the path to your start server script for Minecraft (or other servers) 
+// in the format: 'Minecraft: "<path_to_start_server_script>"'.
 const SERVER_PATH_MAP = {
-  ATM9: '~/Servers/ATM9/startserver.sh',
-  ATM10: '~/Servers/ATM10/startserver.sh',
-}
+  // Example: Minecraft: '~/Server/startserver.sh'
+};
 
 const constants = {
   SERVER_IP, PORT, TIMEOUT, RETRY_INTERVAL,
